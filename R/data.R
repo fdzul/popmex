b1d7c4871c08b1d9beb74e078dcc542f658e8dcc
fdzul <felipe.dzul.m@gmail.com>
2026-03-28 06:39:10 -0600

    This commit update the pkg

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A subset of data from the HUMANITARIAN DATA EXCHANGE Population Density of Mexico
 #' dataset ...
 #'
-#' @format ## `hdx_pop_2022`
+#' @format ## `hdx_pop2022`
 #' A sf & data frame with 463830 rows and 3 columns:
 #' \describe{
 #'   \item{h3}{id of hexagon}
@@ -12,14 +12,14 @@
 #'   ...
 #' }
 #' @source <https://data.humdata.org/dataset/kontur-population-mexico?>
-"hdx_pop_2022"
+"hdx_pop2022"
 
 #' Population Density Mexico HDX data 2023
 #'
 #' A subset of data from the HUMANITARIAN DATA EXCHANGE Population Density of Mexico
 #' dataset ...
 #'
-#' @format ## `hdx_pop_2023`
+#' @format ## `hdx_pop2023`
 #' A sf & data frame with 456514 rows and 3 columns:
 #' \describe{
 #'   \item{h3}{id of hexagon}
@@ -31,14 +31,14 @@
 #' EPSG 4326...
 #'
 #' @source <https://data.humdata.org/dataset/kontur-population-mexico?>
-"hdx_pop_2023"
+"hdx_pop2023"
 
 #' Population Density Mexico HDX data 2021
 #'
 #' A subset of data from the HUMANITARIAN DATA EXCHANGE Population Density of Mexico
 #' dataset ...
 #'
-#' @format ## `hdx_pop_2021`
+#' @format ## `hdx_pop2021`
 #' A sf & data frame with 362028 rows and 2 columns:
 #' \describe{
 #'   \item{popolation}{Population Density for 400m H3 Hexagons}
@@ -49,4 +49,4 @@
 #' EPSG 4326...
 #'
 #' @source <https://data.humdata.org/dataset/kontur-population-dataset>
-"hdx_pop_2021"
+"hdx_pop2021"
